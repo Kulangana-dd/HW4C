11ee93ded0732ecd1480b00c172fbfbd37b872a6
unknown <kulangana@yandex.ru git config --global user.name Kulangana-ddgit --global user.email kulangana@yandex.ru>
2022-10-02 22:45:10 +0300

    task 29

---
+++ task29/Program.cs
@@ -0,0 +1,34 @@
+﻿/*Задача 29: Напишите программу, в которой пользователь задает длину массива,
+ элементы которого задаются в диапазоне [1,99] и выводит на экран.
+    5 -> [1, 2, 5, 7, 19]
+    3 -> [6, 1, 33]*/
+    
+void FillArray (int[] array)
+{
+    int length = array.Length;
+    int index = 0;
+    while (index < length)
+    {
+        array[index] = new Random().Next(1, 100);
+        index++;
+    }
+}
+
+void PrintArray(int[] finalArray)
+{
+    int count = finalArray.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write(finalArray[position]);
+        Console.Write(" ");
+        position++;
+    }
+}
+
+Console.Write("Задайте длину массива: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[number];
+
+FillArray (array);
+PrintArray (array);
